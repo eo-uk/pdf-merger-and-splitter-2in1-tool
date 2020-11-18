bf38258bf1c001ca55f5fa4761954790356d8f01
eo-uk <64873688+eo-uk@users.noreply.github.com>
2020-11-18 16:20:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 from PyPDF2.utils import PdfReadError
 import os
 import sys
+
+
+def merge_pdf(paths):
+    merger = PdfFileMerger()
+    for path in paths:
+        merger.append(path)
+    merger.write(os.path.basename(path) + " - merged.pdf")
+    merger.close()
