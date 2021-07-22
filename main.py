1343c55c6952e54988ad130f0c5be8f645f471c7
eo-uk <64873688+eo-uk@users.noreply.github.com>
2021-07-22 20:36:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,29 @@ def merge_pdf(paths):
         merger.append(path)
     merger.write(os.path.basename(path) + " - merged.pdf")
     merger.close()
+
+def split_pdf(path):
+    '''
+    split_pdf(path)
+
+    Splits a PDF file into separate pages.
+    path: Path to the PDF file.
+    '''
+    #Read the PDF
+    pdf = PdfFileReader(path)
+    #Number of digits in the total number of pages
+    total_digits = len(str(pdf.getNumPages()))
+
+    for i in range(pdf.getNumPages()):
+        #Page number with leading zeroes
+        page_no = str(i+1)
+        page_digits = len(page_no)
+        lead_zeros = '0' * (total_digits - page_digits)
+        #Add page to the writer and write to file
+        writer = PdfFileWriter()
+        writer.addPage(pdf.getPage(i))
+        with open(
+            os.path.basename(path) + " - Page" + lead_zeros + page_no + ".pdf",
+            mode="wb"
+        ) as output:
+            writer.write(output)
